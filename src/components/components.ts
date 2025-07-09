322ab211d23000b2a17d42b2e2fe4e4791802622
Андрей Евтухов <stalker10eg@mail.ru>
2025-07-09 18:54:41 +0300

    add input for searching

--- src/components/components.ts
+++ src/components/components.ts
@@ -6,10 +6,12 @@ export const renderHeader = () => {
   const wrapper = document.createElement('div')
   const title = createTitle('h2', 'Склад')
   const addButton = createButton('submit', 'add', 'Добавить запись')
+  const searchInput = createInput('text', 'search', 'Поиск вещи по названию')
 
   wrapper.className = 'header__wrapper'
+  searchInput.className = 'form__input'
 
-  wrapper.append(title, addButton)
+  wrapper.append(title, searchInput, addButton)
 
   return wrapper
 }
